f63e219e007e795f9d5b7733c3ce86c91b5051b3
HermanT <u7436114@anu.edu.au>
2025-08-10 16:27:31 +1000

    allow all origin for testing purposes

--- apps/backend/src/main/java/com/campusride/config/WebSocketConfig.java
+++ apps/backend/src/main/java/com/campusride/config/WebSocketConfig.java
@@ -20,8 +20,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setAllowedOrigins(
-                    "http://localhost:5173",
-                    "https://campusride.herman-tang.com",
+                    // "http://localhost:5173",
+                    // "https://campusride.herman-tang.com",
                     "*"
                 )
                 .withSockJS();
