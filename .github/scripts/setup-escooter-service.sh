66ee329d37511b4b43ed4f2a95f936d30fc2fdde
HermanT <u7436114@anu.edu.au>
2025-08-11 14:01:36 +1000

    Add python script deployment

---
+++ .github/scripts/setup-escooter-service.sh
@@ -0,0 +1,12 @@
+[Unit]
+Description=Escooter Python Service
+After=network.target
+
+[Service]
+User=ec2-user
+WorkingDirectory=/home/ec2-user/app
+ExecStart=/usr/bin/python3 /home/ec2-user/app/escooter-ec2.py
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
